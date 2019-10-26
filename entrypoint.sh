00a5a59218276f1c46b271c5e5ec48aa789c5e0a
antonio <antonio@morningwall.com>
2019-10-26 13:11:49 +0100

    Adding port 443 and self signed cert

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -eu
 
+
+DIR=/etc/nginx/conf.d/certs
+mkdir -p $DIR
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${DIR}/server.key -out ${DIR}/server.crt -subj "/C=UK/ST=Greater London/L=London/O=AMA Network/OU=IT Department"
 echo "<!DOCTYPE html>
 <html>
   <head>
